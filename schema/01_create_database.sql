f94794688a4634cc23eee8380035cb2064afa68e
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-01-08 15:49:34 -0300

    Database creation code

--- schema/01_create_database.sql
+++ schema/01_create_database.sql
@@ -1,5 +1,10 @@
---Create database
-
+/*
+File: 01_create_database.sql
+Project: Star Systems Database
+Description: Database creation code
+Author: Pedro Henkels
+Date: 2026/01/07
+*/
 CREATE DATABASE StarSystem ON PRIMARY
 (NAME = StarSystem,
 FILENAME = 'C:\SQL\StarSystem\StarSystem.mdf',
