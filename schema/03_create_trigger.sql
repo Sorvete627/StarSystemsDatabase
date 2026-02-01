fa44ff6faebf74c5cf17e6cfc9280a1678d658f8
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-01 14:01:41 -0300

    Triggers creation code

--- schema/03_create_trigger.sql
+++ schema/03_create_trigger.sql
@@ -14,8 +14,8 @@ Date: 2026/01/20
 	NOCOUNT ON is used to avoid visual pollution, since triggers usually affects multiple lines.
 */
 
---Do not allow a total atmosphere of a planet to be more than 100%.
-CREATE TRIGGER TRG_PlanetAtmosphere_AfterInsert_OneHundredPercentAtmosphere
+--Do not allow a total atmosphere of a body to be more than 100%.
+CREATE TRIGGER TRG_PlanetAtmosphere_AfterInsertUpdate_OneHundredPercentAtmosphere
 ON dbo.PlanetAtmosphere
 AFTER INSERT, UPDATE
 AS
@@ -25,18 +25,18 @@ BEGIN
 		JOIN Inserted i
 			ON pa.IdPlanet = i.IdPlanet
 		GROUP BY pa.IdPlanet
---GROUP BY to SUM Ratio once per planet, not all planet's ratio at once
+--GROUP BY to SUM Ratio once per body, not all body ratio at once.
 		HAVING SUM(pa.Ratio) > 100)
 		BEGIN;
 			THROW 50002,	
-				'The total atmosphere of a planet can not be more than 100%.',
+				'The total atmosphere of a body can not be more than 100%.',
 				1
 		END;
 END;
 GO
 
 --Ensure business rule in which a planet can only orbit multiple stars from the same system.
-CREATE TRIGGER TRG_PlanetStar_AfterInsert_SameSystemStars
+CREATE TRIGGER TRG_PlanetStar_AfterInsertUpdate_SameSystemStars
 ON dbo.PlanetStar
 AFTER INSERT, UPDATE
 AS
@@ -58,135 +58,24 @@ BEGIN
 END;
 GO
 
---Ensure business rule in which a planet can only orbit multiple brown dwarfs from the same system.
-CREATE TRIGGER TRG_PlanetBrownDwarf_AfterInsert_SameSystemBrownDwarfs
-ON dbo.PlanetBrownDwarf
-AFTER INSERT, UPDATE
-AS 
-BEGIN
-	SET NOCOUNT ON;
-	IF EXISTS (SELECT 1 FROM PlanetBrownDwarf pb
-		JOIN Inserted i
-			ON pb.IdPlanet = i.IdPlanet
-		JOIN BrownDwarf bd
-			ON pb.IdBrownDwarf = bd.IdBrownDwarf
---GROUP BY to count distinct IdSystem only once from a specific IdPlanet
-		GROUP BY pb.IdPlanet
-		HAVING COUNT(DISTINCT bd.IdSystem) > 1)
-	BEGIN;
-		THROW 50004,
-			'A planet can only orbit two or more brown dwarfs if all are from the same star system.',
-			1;
-	END;
-END;
-GO
-
---Ensure business rule in which brown dwarf can only orbit a star if the brown dwarf is not considered primary.
-CREATE TRIGGER TRG_BrownDwarfStar_AfterInsert_IsBrownDwarfPrimary
-ON dbo.BrownDwarfStar
+--Ensure that a moon dont orbit a star
+CREATE TRIGGER TRG_BodyStar_AfterInsertUpdate_MoonDontOrbitStar
+ON dbo.BodyStar
 AFTER INSERT, UPDATE
 AS
 BEGIN
 	SET NOCOUNT ON;
 	IF EXISTS (SELECT 1 FROM Inserted i
-		JOIN BrownDwarf bd
-			ON i.IdBrownDwarf = bd.IdBrownDwarf
-		WHERE bd.IsPrimary = 1)
+		JOIN Body b
+			ON i.IdBody = b.IdBody
+		JOIN BodyType bt
+			ON b.IdBodyType = bt.IdBodyType
+	WHERE bt.BodyTypeName = 'Moon')
 	BEGIN;
-		THROW 50005,
-			'A brown dwarf which is primary can not orbit a star.',
+		THROW 50003,
+			'A moon can not orbit a star, only planets',
 			1;
 	END;
 END;
 GO
 
---Ensure business rule in which a brown dwarf can only orbit multiple stars from the same system.
-CREATE TRIGGER TRG_BrownDwarfStar_AfterInsert_SameSystemStars
-ON dbo.BrownDwarfStar
-AFTER INSERT, UPDATE
-AS
-BEGIN
-	SET NOCOUNT ON;
-	IF EXISTS (SELECT 1 FROM BrownDwarfStar bs
-		JOIN Inserted i
-			ON bs.IdBrownDwarf = i.IdBrownDwarf
-		JOIN Star s
-			ON bs.IdStar = s.IdStar
-		GROUP BY bs.IdBrownDwarf
---GROUP BY to count distinct IdSystem only once from a specific IdBrownDwarf
-		HAVING COUNT(DISTINCT s.IdSystem) > 1)
-	BEGIN;
-		THROW 50006,
-			'A planet can only orbit two or more brown dwarfs if all are from the same star system.',
-			1;
-	END;
-END;
-GO
-
---Ensure business rule in which a planet can only orbit a brown dwarf if is considered primary.
-CREATE TRIGGER TRG_PlanetBrownDwarf_AfterInsert_IsBrownDwarfPrimary
-ON dbo.PlanetBrownDwarf
-AFTER INSERT, UPDATE
-AS
-BEGIN
-	SET NOCOUNT ON;
-	IF EXISTS (SELECT 1 FROM Inserted i
-		JOIN BrownDwarf bd
-			ON i.IdBrownDwarf = bd.IdBrownDwarf
-		WHERE bd.IsPrimary = 0)
-	BEGIN;
-		THROW 50007,
-			'A planet can only orbit a brown dwarf if it is considered primary.',
-			1;
-	END;
-END;
-GO
-
-/*
-Following two trigger exists to avoid that a planet can be associated to, orbit, a brown dwarf and a star with both being from different systems.
-Since PlanetBrownDwarf and PlanetStar are two independent tables, a INSERT could associate any planet to a star where IdSystem = 1 on table PlanetStar and associate the same planet to a brown dwarf where IdSystem = 2
-*/
-
-CREATE TRIGGER TRG_PlanetStar_AfterInsert_ValidateBrownDwarfSystem
-ON dbo.PlanetStar
-AFTER INSERT, UPDATE
-AS
-BEGIN
-	SET NOCOUNT ON;
-	IF EXISTS (SELECT 1 FROM Inserted i
-		JOIN PlanetBrownDwarf pd
-			ON i.IdPlanet = pd.IdPlanet
-		JOIN BrownDwarf bd
-			ON pd.IdBrownDwarf = bd.IdBrownDwarf
-		JOIN Star s
-			ON i.IdStar = s.IdStar
-		WHERE s.IdSystem <> bd.IdSystem)
-	BEGIN;
-		THROW 50008,
-			'A planet can only orbit a star and a brown dwarf if both are from the same system',
-			1;
-	END;
-END;
-GO
-
-CREATE TRIGGER TRG_PlanetBrownDwarf_AfterInsert_ValidateStarSystem
-ON dbo.PlanetBrownDwarf
-AFTER INSERT, UPDATE
-AS
-BEGIN
-	SET NOCOUNT ON;
-	IF EXISTS (SELECT 1 FROM Inserted i
-		JOIN PlanetStar ps
-			ON i.IdPlanet = ps.IdPlanet
-		JOIN Star s
-			ON  ps.IdStar = s.IdStar
-		JOIN BrownDwarf bd
-			ON i.IdBrownDwarf = bd.IdBrownDwarf
-		WHERE s.IdSystem <> bd.IdSystem)
-	BEGIN;
-		THROW 50009,
-			'A planet can only orbit a star and a brown dwarf if both are from the same system',
-			1;
-	END;
-END;
-GO
