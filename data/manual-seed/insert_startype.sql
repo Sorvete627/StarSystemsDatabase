c0e1c4300b054d2337d34813400ade63b7d07689
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-09 17:40:04 -0300

    Create insert_startype.sql

---
+++ data/manual-seed/insert_startype.sql
@@ -0,0 +1,5 @@
+INSERT INTO StarType (StarTypeName)
+VALUES ('O (Blue-White) Star'), ('B (Blue-White) Star'), ('B Blue-White Super Giant) Star'), ('A (Blue-White) Star'), ('A (Blue-White Super Giant) Star'), ('F (White) Star'),
+('F (White Super Giant) Star'), ('G (White-Yellow) Star'), ('G (White-Yellow Super Giant) Star'), ('K (Yellow-Orange) Star'), ('K (Yellow-Orange Giant) Star'), ('M (Red Dwarf) Star'),
+('M (Red Giant) Star'), ('M (Red Super Giant) Star'), ('T Tauri Star'), ('Herbig Ae/Be Star'), ('Wolf-Rayet Star'), ('Wolf-Rayet N Star'), ('Wolf-Rayet NC Star'), ('Wolf-Rayet C Star'),
+('Wolf-Rayet O Star'), ('Carbon Star'), ('MS-Type Star'), ('S-Type Star'), ('White Dwarf Star'), ('Neutron Star'), ('Black Hole'), ('Supermassive Black Hole');
