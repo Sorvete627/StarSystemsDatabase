8f10e6270ac59a75f0cacbfd8a3096d2da556c10
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-09 17:40:52 -0300

    Create insert_atmosphere.sql

---
+++ data/manual-seed/insert_atmosphere.sql
@@ -0,0 +1,3 @@
+INSERT INTO Atmosphere (AtmosphereName)
+VALUES ('No Atmosphere'), ('Ammonia'), ('Carbon Dioxide'), ('Sulphur Dioxide'), ('Nitrogen'), ('Methane'), ('Helium'), ('Silicate Vapour'), ('Metallic Vapour'), ('Neon'), ('Argon'),
+('Oxygen');
