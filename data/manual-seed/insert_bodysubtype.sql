dbfa8ce84677f4bf8c1f03e9ec5624642e3f0c4a
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-09 15:35:43 -0300

    Create insert_bodysubtype.sql

---
+++ data/manual-seed/insert_bodysubtype.sql
@@ -0,0 +1,4 @@
+INSERT INTO BodySubType (BodySubTypeName)
+VALUES ('Icy'), ('Rocky'), ('Rocky Ice'), ('High Metal Content'), ('Metal-Rich'), ('Ammonia'), ('Water'), ('Earth-Like'), ('Class 1 Gas Giant'), ('Class 2 Gas Giant'),
+('Class 3 Gas Giant'), ('Class 4 Gas Giant'), ('Class 5 Gas Giant'), ('Helium-Rich'), ('Helium Gas Giant'), ('Gas Giant with Ammonia-Based Life'), ('Gas Giant with Water-Based Life'),
+('Water Giant');
