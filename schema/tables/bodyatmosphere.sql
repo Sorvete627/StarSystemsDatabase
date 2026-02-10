c2a1af253e910168ecfc6c2cdff9437ccfca351c
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-10 19:06:41 -0300

    Create bodyatmosphere.sql

---
+++ schema/tables/bodyatmosphere.sql
@@ -0,0 +1,19 @@
+/*
+File: bodyatmosphere.sql
+Objetc dbo.BodyAtmosphere
+Author: Pedro Henkels
+Date: 2026/01/08
+*/
+
+CREATE TABLE dbo.BodyAtmosphere(
+	IdBody INT,
+	IdAtmosphere TINYINT,
+	Ratio DECIMAL (5,2) NOT NULL,
+CONSTRAINT PK_BodyAtmosphere_IdBody_IdAtmosphere
+--Only the pair of body and atmosphere should be unique
+	PRIMARY KEY (IdBody, IdAtmosphere),
+CONSTRAINT FK_BodyAtmosphere_IdBody
+	FOREIGN KEY (IdBody) REFERENCES Body(IdBody) ON DELETE CASCADE,
+CONSTRAINT FK_BodyAtmosphere_IdAtmosphere
+	FOREIGN KEY (IdAtmosphere) REFERENCES Atmosphere(IdAtmosphere) ON DELETE CASCADE);
+GO
