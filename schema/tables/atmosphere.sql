c581845b9a3a82b1749ac7c7ff862f65c2581686
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-10 19:06:04 -0300

    Create atmosphere.sql

---
+++ schema/tables/atmosphere.sql
@@ -0,0 +1,13 @@
+/*
+File: atmosphere.sql
+Objetc dbo.Atmosphere
+Author: Pedro Henkels
+Date: 2026/01/08
+*/
+
+CREATE TABLE dbo.Atmosphere(
+	IdAtmosphere TINYINT IDENTITY (1,1)
+		CONSTRAINT PK_Atmosphere PRIMARY KEY,
+	AtmosphereName VARCHAR(30) NOT NULL 
+		CONSTRAINT UQ_Atmosphere_AtmosphereName UNIQUE);
+GO
