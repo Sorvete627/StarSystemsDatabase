a59c63c858467a9721742f0c8265904577a4d774
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-10 19:03:07 -0300

    Create bodysubtype.sql

---
+++ schema/tables/bodysubtype.sql
@@ -0,0 +1,13 @@
+/*
+File: bodysubtype.sql
+Objetc dbo.BodySubType
+Author: Pedro Henkels
+Date: 2026/01/08
+*/
+
+CREATE TABLE dbo.BodySubType(
+	IdBodySubType TINYINT IDENTITY (1,1)
+		CONSTRAINT PK_BodySubType PRIMARY KEY,
+	BodySubTypeName VARCHAR (30) NOT NULL
+		CONSTRAINT UQ_BodySubType_BodySubTypeName UNIQUE);
+GO
