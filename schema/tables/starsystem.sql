ccb9bf6608d979a2fd8693148281de27a46746be
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-10 18:59:43 -0300

    Create starsystem.sql

---
+++ schema/tables/starsystem.sql
@@ -0,0 +1,14 @@
+/*
+File: starsystem.sql
+Objetc dbo.StarSystem
+Author: Pedro Henkels
+Date: 2026/01/08
+*/
+
+CREATE TABLE dbo.StarSystem(
+	IdStarSystem SMALLINT IDENTITY (1,1)
+		CONSTRAINT PK_System PRIMARY KEY,
+	StarSystemName VARCHAR(100) NOT NULL
+--Star systems with the same name do not exist
+		CONSTRAINT UQ_StarSystem_StarSystemName UNIQUE);
+GO
