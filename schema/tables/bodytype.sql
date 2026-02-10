8110d58b97ee792ed0ab094e4b82673c140dffc9
Pedro Henkels <eu.sorvetequente@gmail.com>
2026-02-10 19:02:18 -0300

    Create bodytype.sql

---
+++ schema/tables/bodytype.sql
@@ -0,0 +1,13 @@
+/*
+File: bodytype.sql
+Objetc dbo.BodyType
+Author: Pedro Henkels
+Date: 2026/01/08
+*/
+
+CREATE TABLE dbo.BodyType(
+	IdBodyType TINYINT IDENTITY (1,1)
+		CONSTRAINT PK_BodyType PRIMARY KEY,
+	BodyTypeName VARCHAR(30) NOT NULL
+	CONSTRAINT UQ_BodyType_BodyTypeName UNIQUE);
+GO
